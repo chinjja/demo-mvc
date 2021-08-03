94794a7afe521dae8e5b5da2a89522f819a1a762
chinjja <chinjja@gmail.com>
2021-08-03 18:47:23 +0900

    DemoInterceptor

--- src/main/java/com/chinjja/app/web/WebConfig.java
+++ src/main/java/com/chinjja/app/web/WebConfig.java
@@ -2,10 +2,18 @@ package com.chinjja.app.web;
 
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ServletComponentScan
 public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+		.addInterceptor(new DemoInterceptor())
+		.addPathPatterns("/api/users/*");
+	}
 
 }
